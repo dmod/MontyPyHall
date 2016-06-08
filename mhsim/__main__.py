7667fd3ab517f0ed76964e52e9ea1d1c4a0062eb
Andrew <adarmody@gmail.com>
2016-06-08 00:21:21 -0400

    Program restructure
    Made MontyHallGame() a subclass of multiprocessing.Process, removed
    results Queue (replaced with multiprocessing.Value), code cleanup

--- mhsim/__main__.py
+++ mhsim/__main__.py
@@ -1,10 +1,11 @@
 import multiprocessing
 import signal
 import traceback
+import time
 
 import montyhallgame
 
-NUM_PROCESSES = 3
+NUM_PROCESSES = 4
 
 class MontyHallSim:
 
@@ -15,42 +16,37 @@ class MontyHallSim:
 
     def shutdown(self, *args):
         for game in self.games:
-            game.shutdown()
+            game.terminate()
 
         self.shutdownFlag = True
 
     # For the number of processes defined, start a worker process that will
     # continually run a simulation of the Montly Hall Game
     def startWorkers(self):
-        self.resultsQueue = multiprocessing.Queue()
-
         self.games = []
 
-        for i in range(NUM_PROCESSES):
-            game = montyhallgame.MontyHallGame(self.resultsQueue)
-
-            workerProcess = multiprocessing.Process(target=game.start)
-            workerProcess.start()
-            self.games.append(workerProcess)
+        for _ in range(NUM_PROCESSES):
+            game = montyhallgame.MontyHallGame()
+            game.start()
+            self.games.append(game)
 
     def collectResults(self):
-        switchWins = 0
-        switchLosses = 0
 
-        counter = 0
-        
         while not self.shutdownFlag:
-            counter = counter + 1
-            result = self.resultsQueue.get()
-            if result:
-                switchWins = switchWins + 1
-            else:
-                switchLosses = switchLosses + 1
-            
-            switchWinPercentage = switchWins / (switchLosses + switchWins)
-            if ((counter % 100000) == 0):
-               print("queue size: " + str(self.resultsQueue.qsize()) + " switchWins : " + str(switchWins) + " switch losses " + str(switchLosses) + " percentage " + str(switchWinPercentage), end='\r')
 
+            collectiveSwitchWins = 0
+            collectiveSwitchLosses = 0
+
+            for game in self.games:
+                switchWins, switchLosses = game.getCurrentResults()
+
+                with switchWins.get_lock():
+                    collectiveSwitchWins += switchWins.value
+                with switchLosses.get_lock():
+                    collectiveSwitchLosses += switchLosses.value
+
+            print("Switch Wins: " + str(collectiveSwitchWins) +
+                  " Switch Losses: " + str(collectiveSwitchLosses), end='\r')
 
 if __name__ == "__main__":
     # execute only if run as a script
--- mhsim/montyhallgame.py
+++ mhsim/montyhallgame.py
@@ -1,44 +1,66 @@
 import random
 import signal
+import multiprocessing
 from enum import Enum
 
 NUM_DOORS = 3
 
 Door = Enum('Door', 'goat car')
 
-class MontyHallGame():
+class MontyHallGame(multiprocessing.Process):
 
-    def __init__(self, resultsQueue):
+    def __init__(self):
+        super(MontyHallGame, self).__init__()
+
         self.shutdownFlag = False
-        signal.signal(signal.SIGINT, self.shutdown)
         signal.signal(signal.SIGTERM, self.shutdown)
 
-        self.resultsQueue = resultsQueue
+        self.switchWins = multiprocessing.Value('i', 0)
+        self.switchLosses = multiprocessing.Value('i', 0)
 
     def shutdown(self, *args):
         self.shutdownFlag = True
 
     def goGame(self):
+
         doors = []
+        # The car will be placed at a random index between 0 and NUM_DOORS
         carIndex = random.randrange(0, NUM_DOORS)
 
-        for index in range(NUM_DOORS):
-            if index == carIndex:
+        # Iterate through the NUM_DOORS and populate each door with either a
+        # car or a goat depending on the index of the door
+        for doorIndex in range(NUM_DOORS):
+            if doorIndex == carIndex:
                 doors.append(Door.car)
             else:
                 doors.append(Door.goat)
 
-        myPick = random.randrange(0, NUM_DOORS)
+        # Doors are now populated, have the contestant pick a random door index
+        contestantPick = random.randrange(0, NUM_DOORS)
 
-        for index in range(NUM_DOORS):
-            if index != myPick and doors[index] is not Door.car:
-                montyPick = index
+        # Now that the contestant has picked their door choice, have Monty pick
+        # a door that 1) The contestant did not pick AND 2) Does not have a car
+        # behind it (Monty knows which door has a car)
+        for doorIndex in range(NUM_DOORS):
+            if doorIndex != contestantPick and doors[doorIndex] is not Door.car:
+                montyPick = doorIndex
 
-        for index in range(NUM_DOORS):
-            if index != myPick and index != montyPick:
-                return (doors[index] is Door.car)
+        # Now, since this contestant will always switch their choice, iterate
+        # through the doors to find the door that neither the contestant nor
+        # Monty picked (the door that the contestant would have switched to)
+        # and return True if the door contained the car
+        for doorIndex in range(NUM_DOORS):
+            if doorIndex != contestantPick and doorIndex != montyPick:
+                return (doors[doorIndex] is Door.car)
 
+    def getCurrentResults(self):
+        return (self.switchWins, self.switchLosses)
 
-    def start(self):
+    def run(self):
         while not self.shutdownFlag:
-            self.resultsQueue.put(self.goGame())
+            if self.goGame():
+                with self.switchWins.get_lock():
+                    self.switchWins.value += 1
+            else:
+                with self.switchLosses.get_lock():
+                    self.switchLosses.value += 1
